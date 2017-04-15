e19b8a5552f53dd543c897a62ba9504e1ea2fe73
Guillaume Chanson <guillaume.chns@gmail.com>
2017-04-15 14:58:17 +0200

    Main entry point for everything

---
+++ src/Main.kt
@@ -0,0 +1,11 @@
+/**
+ * Let's playyyyyyyyyyy.
+ * Run it and let the magic happen.
+ */
+fun main(args: Array<String>) {
+    val game = LeCompteEstBon.create()
+    game.printStatement()
+
+    val solver = Solver(game.toGuess, game.numbers.toIntArray())
+    solver.naiveAlgorithm()
+}
